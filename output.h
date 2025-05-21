3e9b0fc5938ca4ffb83d45d1268e458baeb7b2d2
katherineChen0 <katherine05chen@gmail.com>
2025-05-21 13:27:33 -0700

    output added

---
+++ output.h
@@ -0,0 +1,12 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#include <stdbool.h>
+
+bool initialize_output(const struct options *opts);
+bool write_bytes(unsigned long long x, int nbytes);
+void finalize_output(void);
+bool flush_remaining_bytes(void);
+
+#endif
+
